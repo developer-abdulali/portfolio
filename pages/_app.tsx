596f3c745b46fa47210cd82b9922faafab969a9b
developer-abdulali <ab.ali.soomro@gmail.com>
2023-07-30 17:05:34 +0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import Navbar from "../components/navbar/navbar";
+import Navbar from "@/components/navbar/navbar";
 const App =({ Component, pageProps }: AppProps) => {
   return (
     <>
